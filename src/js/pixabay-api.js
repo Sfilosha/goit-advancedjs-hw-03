113bc23f4eeadbe7d16bc598552d5a8728603b64
Sfilosha <sfilosha@gmail.com>
2025-01-12 15:10:43 +0200

    'pixabay-api.js' refactoring

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,12 @@
-const apiParams = {
+const apiParams = new URLSearchParams({
     key: "10224742-53a1a7880f946c3462445b43a",
     image_type: "photo",
     orientation: "horizontal",
     safesearch: "true"
-}
-
-const parametersString = new URLSearchParams(apiParams).toString();
+})
 
 export const getImages = searchQuery => {
-    return fetch(`https://pixabay.com/api/?${parametersString}&q=${searchQuery}`)
+    return fetch(`https://pixabay.com/api/?${apiParams}&q=${searchQuery}`)
     .then(response => {
         if (!response.ok) {
             throw new Error(response.status);
